dd9dd500889c50f255cb783ade6e3e68f97e7f41
Developer <dev@MBP13i5.fritz.box>
2018-11-25 22:27:06 +0100

    cool

---
+++ W3Cx/HTML5_2x/1_5_5/main.js
@@ -0,0 +1,51 @@
+//buil an equalizer with multiple biquad filters
+
+var ctx;
+var context;
+
+var mediaElement;
+var sourceNode;
+
+// create the equalizer. It's a set of biquad Filters
+
+var filters;
+
+function onload() {
+    ctx = window.AudioContext || window.webkitAudioContext;
+    context = new ctx();
+
+    mediaElement = document.getElementById('player');
+    sourceNode = context.createMediaElementSource(mediaElement);
+
+    // create the equalizer. It's a set of biquad Filters
+
+    filters = [];
+
+    // Set filters
+    [60, 170, 350, 1000, 3500, 10000].forEach(function (freq, i) {
+        var eq = context.createBiquadFilter();
+        eq.frequency.value = freq;
+        eq.type = "peaking";
+        eq.gain.value = 0;
+        filters.push(eq);
+    });
+
+    // Connect filters in serie
+    sourceNode.connect(filters[0]);
+    for (var i = 0; i < filters.length - 1; i++) {
+        filters[i].connect(filters[i + 1]);
+    }
+
+    // connect the last filter to the speakers
+    filters[filters.length - 1].connect(context.destination);
+
+}
+
+function changeGain(sliderVal, nbFilter) {
+    var value = parseFloat(sliderVal);
+    filters[nbFilter].gain.value = value;
+
+    // update output labels
+    var output = document.querySelector("#gain" + nbFilter);
+    output.value = value + " dB";
+}
