d59908a350847844457dcae18d511861229394c3
Developer <dev@MBP13i5.local>
2018-11-28 21:22:23 +0100

    game skeletton with keyboard / mouseevents

--- W3Cx/HTML5_2x/_theGame/main.js
+++ W3Cx/HTML5_2x/_theGame/main.js
@@ -5,6 +5,13 @@ var GF = function () {
     var fpsContainer;
     var fps;
 
+    // var for canvas
+    var ctx, canvas;
+    var w, h;
+
+    // vars for handling inputs
+    var inputStates = {};
+
     var measureFPS = function (newTime) {
 
         // test for the very first invocation
@@ -32,25 +39,130 @@ var GF = function () {
         //main function, called each frame 
         measureFPS(time);
 
+        // canvas part
+        clearCanvas(ctx, w, h);
+
+        // draw my rect
+        drawMyRect(10 + Math.random() * 10, 10 + Math.random() * 10);
+
+        // check inputStates
+        if (inputStates.left) {
+            ctx.fillText("left", 150, 20);
+        }
+        if (inputStates.up) {
+            ctx.fillText("up", 150, 50);
+        }
+        if (inputStates.right) {
+            ctx.fillText("right", 150, 80);
+        }
+        if (inputStates.down) {
+            ctx.fillText("down", 150, 120);
+        }
+        if (inputStates.space) {
+            ctx.fillText("space bar", 140, 150);
+        }
+
+        if (inputStates.mousePos) {
+            ctx.fillText("x = " + inputStates.mousePos.x + " y = " +
+                inputStates.mousePos.y, 5, 150);
+        }
+        if (inputStates.mousedown) {
+            ctx.fillText("mousedown b" + inputStates.mouseButton, 5, 180);
+        }
         // call the animation loop every 1/60th of second
         requestAnimationFrame(mainLoop);
     };
+
+    function getMousePos(evt) {
+        // Necessary to take into account CSS boudaries
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
 
     var start = function () {
         // adds a div for displaying the fps value
         fpsContainer = document.createElement('div');
         document.body.appendChild(fpsContainer);
 
+        canvas = document.body.querySelector("#myCanvas");
+        w = canvas.width;
+        h = canvas.height;
+
+        ctx = canvas.getContext('2d');
+
+        // Default police for text
+        ctx.font = "20px Arial";
+        // Add the listener to the main, window object, and update the states
+        window.addEventListener('keydown', function (event) {
+            if (event.keyCode === 37) {
+                inputStates.left = true;
+            } else if (event.keyCode === 38) {
+                inputStates.up = true;
+            } else if (event.keyCode === 39) {
+                inputStates.right = true;
+            } else if (event.keyCode === 40) {
+                inputStates.down = true;
+            } else if (event.keyCode === 32) {
+                inputStates.space = true;
+            }
+        }, false);
+
+        // If the key is released, change the states object
+        window.addEventListener('keyup', function (event) {
+            if (event.keyCode === 37) {
+                inputStates.left = false;
+            } else if (event.keyCode === 38) {
+                inputStates.up = false;
+            } else if (event.keyCode === 39) {
+                inputStates.right = false;
+            } else if (event.keyCode === 40) {
+                inputStates.down = false;
+            } else if (event.keyCode === 32) {
+                inputStates.space = false;
+            }
+        }, false);
+
+        // Mouse event listeners
+        canvas.addEventListener('mousemove', function (evt) {
+            inputStates.mousePos = getMousePos(evt);
+        }, false);
+        canvas.addEventListener('mousedown', function (evt) {
+            inputStates.mousedown = true;
+            inputStates.mouseButton = evt.button;
+        }, false);
+        canvas.addEventListener('mouseup', function (evt) {
+            inputStates.mousedown = false;
+        }, false);
+
         requestAnimationFrame(mainLoop);
     };
 
+    function clearCanvas(ctx, w, h) {
+        ctx.clearRect(0, 0, w, h);
+    }
+
+    function drawMyRect(x, y) {
+
+        ctx.save();
+
+        ctx.translate(x, y);
+
+        ctx.strokeRect(0, 0, 100, 100);
+
+        ctx.fillRect(0, 0, 50, 50)
+
+        ctx.restore();
+    }
     //our GameFramework returns a public API visible from outside its scope
     return {
         start: start
     };
 };
 
-window.onload = function init(){
+window.onload = function init() {
     var game = new GF();
     game.start();
 }
