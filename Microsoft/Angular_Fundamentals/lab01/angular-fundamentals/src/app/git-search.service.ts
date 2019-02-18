5d5943306ff12d83f1e30393946ed9b39a8e7848
Developer <dev@MBP13i5.fritz.box>
2019-02-18 08:27:17 +0100

    angular fundamentals

---
+++ Microsoft/Angular_Fundamentals/lab01/angular-fundamentals/src/app/git-search.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { GitSearch } from './git-search';
+@Injectable({
+  providedIn: 'root'
+})
+export class GitSearchService {
+
+  cachedValues: Array<{
+    [query: string]: GitSearch
+  }> = [];
+  constructor() { }
+}
