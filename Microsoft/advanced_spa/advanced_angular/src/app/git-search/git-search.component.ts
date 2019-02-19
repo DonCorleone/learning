0908370b142e9c149dc746b5be00ee82c309e7be
Developer <dev@Administrators-MacBook-Pro.local>
2019-02-19 21:22:42 +0100

    reactive forms validation

--- Microsoft/advanced_spa/advanced_angular/src/app/git-search/git-search.component.ts
+++ Microsoft/advanced_spa/advanced_angular/src/app/git-search/git-search.component.ts
@@ -3,7 +3,7 @@ import { GitSearchService } from '../git-search.service'
 import { GitSearch } from '../git-search'
 import { ActivatedRoute, ParamMap, Router } from '@angular/router'
 import { AdvancedSearchModel } from '../advanced-search-model';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-git-search',
@@ -18,22 +18,35 @@ export class GitSearchComponent implements OnInit {
   form: FormGroup;
   formControls = {};
 
-  constructor(
-    private GitSearchService: GitSearchService,
-    private route: ActivatedRoute,
-    private router: Router
-  ) { 
-
-    this.modelKeys.forEach(elem => {
-      this.formControls[elem] = new FormControl();
+  constructor(private GitSearchService: GitSearchService, private route: ActivatedRoute, private router: Router ) {
+    this.modelKeys.forEach( (key) => {
+        let validators = [];
+        if (key === 'q') {
+            validators.push(Validators.required);
+        }
+        if (key === 'stars') {
+            validators.push(Validators.maxLength(4));
+        }
+        validators.push(this.noSpecialChars);
+        this.formControls[key] = new FormControl(this.model[key], validators);
     })
-
     this.form = new FormGroup(this.formControls);
-  }
+}
+
 
   model = new AdvancedSearchModel('', '', '', null, null, '');
   modelKeys = Object.keys(this.model);
 
+  noSpecialChars(c: FormControl){
+
+    let REGEXP = new RegExp(/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/);
+
+    return REGEXP.test(c.value) ? {
+      validatEmail:{
+        valid: false
+      }
+    } : null;
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
